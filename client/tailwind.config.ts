dbd1c0ddc4c208e64aa3df25e83e567e2e08f610
Ben Hooper <ben@labrys.io>
2024-05-25 16:52:52 +0200

    feat: made font better

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -13,6 +13,19 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+
+      fontFamily: {
+        sans: [
+          "ui-monospace",
+          "SFMono-Regular",
+          "Menlo",
+          "Monaco",
+          "Consolas",
+          "Liberation Mono",
+          "Courier New",
+          "monospace",
+        ],
+      },
     },
   },
   plugins: [],
