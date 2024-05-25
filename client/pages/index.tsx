42493cb197f3c4a12f493f6f6648116eda7a1705
Ben Hooper <ben@labrys.io>
2024-05-25 17:10:37 +0200

    fix: added tab title

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,16 +1,25 @@
 "use client";
 
-import Divider from "@/components/divider";
 import Header from "@/components/header";
 import Register from "@/components/register";
 import UserRewards from "@/components/user-rewards";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <main className={`flex min-h-screen flex-col justify-between p-12`}>
-      <Header />
-      <Register />
-      <UserRewards />
-    </main>
+    <>
+      <Head>
+        <title>zkOfficer</title>
+        <meta
+          name="description"
+          content="digitally prove your physical documents"
+        />
+      </Head>
+      <main className={`flex min-h-screen flex-col justify-between p-12`}>
+        <Header />
+        <Register />
+        <UserRewards />
+      </main>
+    </>
   );
 }
